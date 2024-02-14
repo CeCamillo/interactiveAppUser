922c387b6f0394a237f800d1d0440e18e93e4df5
Joao-Natale <122991781+Joao-Natale@users.noreply.github.com>
2024-02-14 16:54:57 -0300

    começo da comunicação das duas aplicações
    começo da comunicação das duas aplicações

---
+++ components/ui/container.tsx
@@ -0,0 +1,15 @@
+interface ContainerPros {
+    children: React.ReactNode;
+}
+
+const Container: React.FC<ContainerPros> = ({
+    children
+}) => {
+    return (
+        <div className="mx-auto max-w-7xl">
+            {children}
+        </div>
+    );
+}
+
+export default Container;
---
+++ types.ts
@@ -0,0 +1,11 @@
+export interface Company {
+    id: string;
+    name: string;
+    imageUrl: string;
+};
+
+export interface Employee{
+    id: string;
+    name: string;
+    company: Company;
+};
